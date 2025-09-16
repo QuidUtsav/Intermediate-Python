aaa1921913c4f05e4185f948d490d28874933b52
QuidUtsav <122545941+QuidUtsav@users.noreply.github.com>
2025-09-16 11:31:31 +0545

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+
+
+year = [1950, 1960, 1970, 1980, 1990, 2000, 2010,2020]
+pop = [2.519, 3.692, 4.063, 4.434, 5.263, 6.081, 6.972,7.858]
+
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
